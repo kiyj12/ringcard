1ebd154294852ef341fd0232f0da4137402ebbe0
kiyj12 <kiyj12@gmail.com>
2022-11-13 05:52:47 +0900

    feat: make scrollbar hidden

--- ringcard_frontend/src/pages/home/HomeUnanswered.tsx
+++ ringcard_frontend/src/pages/home/HomeUnanswered.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import "../../styles/home.scss";
+import "../../styles/layout.scss";
 import { ThemeProvider } from "react-bootstrap";
 
 const HomeUnanswered = () => {
