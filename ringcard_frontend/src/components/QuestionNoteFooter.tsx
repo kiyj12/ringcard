a9c0cce4c7ea9700f066ba2293aee8596faf6e3c
kiyj12 <kiyj12@gmail.com>
2022-11-17 02:04:18 +0900

    feat:create all handleClick for NoteFooter

--- ringcard_frontend/src/components/QuestionNoteFooter.tsx
+++ ringcard_frontend/src/components/QuestionNoteFooter.tsx
@@ -16,6 +16,9 @@ function FooterUnansweredQuestion(props: FooterProps) {
 		await axios
 			.get("/question/" + questionId + "/delete/question")
 			.then((res) => {
+				console.log(
+					"Successfully enter handleTrashcanClick in QuestionNoteFooter :D"
+				);
 				console.log(res.data);
 				window.history.go(0); // referrer로 이전 페이지를 받아오면 안 된다. 현재 페이지를 해야됨!!!
 			})
@@ -29,7 +32,11 @@ function FooterUnansweredQuestion(props: FooterProps) {
 		await axios
 			.get("/question/" + questionId + "/restore/question")
 			.then((res) => {
+				console.log(
+					"Successfully enter handleRestoreClick in QuestionNoteFooter :D"
+				);
 				console.log(res.data);
+
 				window.history.go(0);
 			})
 			.catch((err) => {
@@ -42,8 +49,56 @@ function FooterUnansweredQuestion(props: FooterProps) {
 		await axios
 			.get("/question/" + questionId + "/inCollection")
 			.then((res) => {
+				console.log(
+					"Successfully enter handleCollectionClick in QuestionNoteFooter :D"
+				);
 				console.log(res.data);
 				window.history.go(0);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	};
+
+	const handleSendAnswerClick = async () => {
+		const questionId = String(question.id);
+		await axios
+			.get("/question/" + questionId + "/unanswered/user")
+			.then((res) => {
+				console.log(
+					"Successfully enter handleSendAnswerClick in QuestionNoteFooter :D"
+				);
+				console.log(res.data);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	};
+
+	const handleEditAnswerClick = async () => {
+		const questionId = String(question.id);
+		await axios
+			.get("/question/" + questionId + "/edit/user")
+			.then((res) => {
+				console.log(
+					"Successfully enter handleEditAnswerClick in QuestionNoteFooter :D"
+				);
+				console.log(res.data);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	};
+
+	const handleDeleteAnswerClick = async () => {
+		const questionId = String(question.id);
+		await axios
+			.get("/question/" + questionId + "/deleteAnswer")
+			.then((res) => {
+				console.log(
+					"Successfully enter handleDeleteAnswerClick in QuestionNoteFooter :D"
+				);
+				console.log(res.data);
 			})
 			.catch((err) => {
 				console.log(err);
@@ -54,21 +109,24 @@ function FooterUnansweredQuestion(props: FooterProps) {
 		<>
 			<div className="note-footer">
 				<div className="note-footer-leftside-btns-container">
-					<button onClick={handleTrashcanClick}>
-						{question.inTrash === false ? (
+					{question.inTrash === false ? (
+						<button onClick={handleTrashcanClick}>
 							<img
 								className="note-trashcan"
 								src="/buttons/note-trashcan-btn.svg"
 								alt=""
 							/>
-						) : (
+						</button>
+					) : (
+						<button onClick={handleRestoreClick}>
 							<img
 								className="note-trashcan"
 								src="/buttons/restore-from-trashcan-button.svg"
 								alt=""
 							/>
-						)}
-					</button>
+						</button>
+					)}
+
 					<button onClick={handleCollectionClick}>
 						{question.inCollection === false ? (
 							<img
@@ -87,7 +145,7 @@ function FooterUnansweredQuestion(props: FooterProps) {
 				</div>
 				<div className="note-footer-rightside-btns-container">
 					{question.answered === false ? (
-						<button>
+						<button onClick={handleSendAnswerClick}>
 							<img
 								className="note-send-answer"
 								src="/buttons/send-answer-btn.svg"
@@ -96,14 +154,14 @@ function FooterUnansweredQuestion(props: FooterProps) {
 						</button>
 					) : (
 						<React.Fragment>
-							<button>
+							<button onClick={handleEditAnswerClick}>
 								<img
 									className="note-edit-answer"
 									src="/buttons/edit-answer-btn.svg"
 									alt=""
 								/>
 							</button>
-							<button onClick={handleRestoreClick}>
+							<button onClick={handleDeleteAnswerClick}>
 								<img
 									className="note-delete-answer"
 									src="/buttons/delete-answer-btn.svg"
