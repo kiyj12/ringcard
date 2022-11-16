90e74e77293f21b3661cccc80c4867405944ae36
yulemu <yule.gpark@gmail.com>
2022-11-17 01:56:39 +0900

    feat editPassword

--- ringcard_frontend/src/pages/mypage/EditPassword.tsx
+++ ringcard_frontend/src/pages/mypage/EditPassword.tsx
@@ -14,30 +14,31 @@ const EditPassword = () => {
 			<div>
 				<div className="editPassword-box">
 					<div className="editPassword-box-in">
-						<div className="editPassword-tag">이름</div>
-						<input className="editPassword-input-user"></input>
+						<div className="editPassword-tag">현재 비밀번호</div>
+						<input className="editPassword-input-pw" placeholder="현재 비밀번호"></input>
 					</div>
 					{/* <i if="${param.overlappedUsername}" text="'이미 존재하는 아이디입니다.'"></i> */}
 					<div className="editPassword-box-in">
-						<div className="editPassword-tag">아이디</div>
-						<input className="editPassword-input-id"></input>
+						<div className="editPassword-tag">새 비밀번호</div>
+						<input className="editPassword-input-pw-new" placeholder="새 비밀번호"></input>
 					</div>
 					<div className="editPassword-box-in">
-						<div className="editPassword-tag">비밀번호</div>
-						<input className="editPassword-input-pw"></input>
-						<button className="editPassword-input-pw-edit-button"><div className="editPassword-input-pw-edit-button-tag">변경</div></button>
+						<div className="editPassword-tag">새 비밀번호 확인</div>
+						<input className="editPassword-input-pw-new-confirm" placeholder="새 비밀번호 확인"></input>
 					</div>
-					<div className="editPassword-box-in">
-						<div className="editPassword-tag">이메일</div>
-						<input className="editPassword-input-email"></input>
-					</div>
-					<div className="editPassword-box-in">
+					<div className="edistPassword-box-in">
 						<button className="editPassword-button">
-						<div className="editPassword-button-tag">변경 사항 저장하기</div>
+						<div className="editPassword-button-tag">비밀번호 변경하기</div>
+						</button>
+					</div>
+					<div className="editPassword-box-in">
+						<button className="editPassword-button-cancel">
+						<div className="editPassword-button-tag-cancel">비밀번호 변경 취소하기</div>
 						</button>
 					</div>
 				</div>
 			</div>
+			{/* 여기에 비밀번호 찾기?? */}
 			{/* <div className="editPassword-delete-box">
 				<div className="editPassword-delete">
 					<a className="editPassword-delete-tag" href="/">링카 계정을 완전히 지우고 싶어요</a>
