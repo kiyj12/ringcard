c001dea9ea0b7b8354239018bd36e524ba2c36e7
kiyj12 <kiyj12@gmail.com>
2022-11-17 04:16:26 +0900

    feat:(front) when home button clicked, move to home/unanswered

--- ringcard_frontend/src/components/Header.tsx
+++ ringcard_frontend/src/components/Header.tsx
@@ -1,15 +1,32 @@
+import axios from "axios";
 import "../styles/layout/header.css";
 
 function Header() {
+	const handleHomeBtnClick = async () => {
+		await axios
+			.get("/home/unanswered")
+			.then((res) => {
+				console.log(res.data);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	};
+
 	return (
 		<header className="header">
 			<div className="header-blank"></div>
 			<div className="top-banner">
-				<img alt="" src="/buttons/home-icon.svg" width="21px" color="white" />
-				<div className="profile-pic">
-					<img alt="" src="/profile.png" width="35px" color="white" />
+				<div className="home-btn" onClick={handleHomeBtnClick}>
+					<img src="/buttons/home-icon.svg" width="21px" color="white" alt="" />
 				</div>
-				<img alt="" src="/buttons/menu-icon.svg" width="21px" color="white" />
+
+				<div className="profile-pic">
+					<img src="/profile.png" width="35px" color="white" alt="" />
+				</div>
+				<div className="menu-btn">
+					<img src="/buttons/menu-icon.svg" width="21px" color="white" alt="" />
+				</div>
 			</div>
 		</header>
 	);
