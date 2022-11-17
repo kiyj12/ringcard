fbe236a7805cf2ef55d4e48954b76339bc789aa7
kiyj12 <kiyj12@gmail.com>
2022-11-18 02:33:13 +0900

    feat:(front) add Link to home and modal to HeaderNoProfile

--- ringcard_frontend/src/components/HeaderNoProfile.tsx
+++ ringcard_frontend/src/components/HeaderNoProfile.tsx
@@ -1,13 +1,43 @@
+import { useState } from "react";
 import "../styles/layout/headerNoProfile.css";
+import Modal from "./Modal";
 
 function HeaderUser() {
+	const handleHomeBtnClick = async () => {
+		window.location.href = "/home/unanswered";
+	};
+
+	// 모달창 노출 여부 state
+	const [showReq, setShowReq] = useState<boolean>(false);
+
+	function openReq() {
+		setShowReq(!showReq);
+	}
+
+	function closeReq() {
+		setShowReq(!showReq);
+	}
+
 	return (
 		<div className="header-user">
 			<div className="header-blank-user"></div>
 			<div className="top-banner-user">
-				<img alt="" src="/buttons/home-icon.svg" width="21px" color="white" />
-				<img alt="" src="/buttons/menu-icon.svg" width="21px" color="white" />
+				<img
+					alt=""
+					src="/buttons/home-icon.svg"
+					width="21px"
+					color="white"
+					onClick={handleHomeBtnClick}
+				/>
+				<img
+					alt=""
+					src="/buttons/menu-icon.svg"
+					width="21px"
+					color="white"
+					onClick={openReq}
+				/>
 			</div>
+			<Modal open={showReq} close={closeReq} />
 			<div className="profile-pic-large-top-blank"></div>
 			<div className="profile-pic-large">
 				<img alt="" src="/profile.png" width="77px" color="white" />
