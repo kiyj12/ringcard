8e12d78612afedead9ba8011589c2789f974ca65
yulemu <yule.gpark@gmail.com>
2022-12-06 08:54:58 +0900

    fix:(back)EditUserInfo UserRingcardName & UserEmail

--- ringcard_backend/src/main/java/com/oneao/ringcard_backend/domain/user/SpringDataJpaUserRepository.java
+++ ringcard_backend/src/main/java/com/oneao/ringcard_backend/domain/user/SpringDataJpaUserRepository.java
@@ -7,5 +7,6 @@ import java.util.Optional;
 
 public interface SpringDataJpaUserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
+    Optional<User> findByUserEmail(String userEmail);
 
 }
--- ringcard_backend/src/main/java/com/oneao/ringcard_backend/domain/user/UserEmailUpdateDto.java
+++ ringcard_backend/src/main/java/com/oneao/ringcard_backend/domain/user/UserEmailUpdateDto.java
@@ -6,21 +6,9 @@ import lombok.Data;
 public class UserEmailUpdateDto {
 //    private String username;
 //    private String password;
-    private String userRingcardName;
+//    private String userRingcardName;
     private String userEmail;
 //    private String twitterId;
 //    private String userPicture;
 ////
-//    public UserUpdateDto() {
-//    }
-//
-//
-//    public UserUpdateDto(String username, String password, String userRingcardName, String userEmail, String twitterId, String userPicture) {
-//        this.username = username;
-//        this.password = password;
-//        this.userRingcardName = userRingcardName;
-//        this.userEmail = userEmail;
-//        this.twitterId = twitterId;
-//        this.userPicture = userPicture;
-//    }
 }
